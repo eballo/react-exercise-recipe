c4eea9c6ef6236314eae511f8aea7ea97c5f227a
eballo <enric.ballo@travelperk.com>
2020-03-25 17:50:46 +0100

    add Style for Recipe and List recipe

--- recipe-app/src/components/Recipe.js
+++ recipe-app/src/components/Recipe.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
+import './Recipe.css';
 
 class Recipe extends Component{
+    static defaultProps = {
+        id: 1,
+        title: "Pizza Motzarella",
+        description: "From Italy with love this is an awesome pizza that no one should miss",
+        ingredients: ["eggs", "oil", "salt"]
+    }
+    contructor(props){
+        this.super(props);
+    }
+
     render(){
-        return(<div>This is a Recipe</div>);
+    return(
+            <div className="Recipe-container">
+                <div className="Recipe-card u-clearfix">
+                    <div className="Recipe-card-body">
+                        
+                        <h2 className="Recipe-card-title">{this.props.title}</h2>
+                        <span class="Recipe-card-description subtle">{this.props.description}</span>
+                        <div class="Recipe-card-read">Read</div>
+                            {this.props.ingredients.map((p) =>(
+                                <span className="subtle">{p} </span>
+                            ))}
+                    </div>
+                </div>
+                <div class="Recipe-card-shadow"></div>
+            </div>
+            );
     }
 }
 
--- recipe-app/src/components/RecipeList.js
+++ recipe-app/src/components/RecipeList.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
+import './RecipeList.css'
 import Recipe from './Recipe.js'
 
 class RecipeList extends Component{
     render(){
         return(
-                <div>
-                    <h1>List Of Recipes</h1>
-                    <Recipe/>
-                    <Recipe/>
-                    <Recipe/>
+            <div>
+                <h1>All My favourites dishes</h1>
+                <div className="RecipeList-container">  
+                    <ul>
+                        <li><Recipe/></li>
+                        <li><Recipe/></li>
+                        <li><Recipe/></li>
+                    </ul>
                 </div>
+            </div>
             );
     }
 }
