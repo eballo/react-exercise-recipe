c5481e20af1b0a75366efa119a09601aa4e18e92
eballo <enric.ballo@travelperk.com>
2020-04-13 11:10:11 +0200

    RecipeList to functional

--- recipe-app/src/components/RecipeList.js
+++ recipe-app/src/components/RecipeList.js
@@ -2,29 +2,25 @@ import React, { Component } from 'react';
 import './RecipeList.css';
 import Recipe from './Recipe.js';
 
-
-class RecipeList extends Component{
-
-    render(){
-        return(
-                <div className="RecipeList-container">  
-                    <ul>
-                    {this.props.recipes.map(j =>(
-                            <li key={j.id}>
-                                <Recipe key={j.id} 
-                                        id={j.id} 
-                                        name={j.name} 
-                                        description={j.description} 
-                                        ingredients={j.ingredients}
-                                        handleRecipeChange={this.props.handleRecipeChange} 
-                                        handleRecipeDelete={this.props.handleRecipeDelete}
-                                        />
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            );
-    }
+function RecipeList(props){
+    return(
+            <div className="RecipeList-container">  
+                <ul>
+                {props.recipes.map(j =>(
+                        <li key={j.id}>
+                            <Recipe key={j.id} 
+                                    id={j.id} 
+                                    name={j.name} 
+                                    description={j.description} 
+                                    ingredients={j.ingredients}
+                                    handleRecipeChange={props.handleRecipeChange} 
+                                    handleRecipeDelete={props.handleRecipeDelete}
+                                    />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
 }
 
 export default RecipeList;
