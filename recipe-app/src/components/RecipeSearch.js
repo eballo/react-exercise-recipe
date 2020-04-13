88efe71a3d8f19f6cd374d122196550a856f3e9f
eballo <enric.ballo@travelperk.com>
2020-04-13 11:03:14 +0200

    recipeSearch to functional

--- recipe-app/src/components/RecipeSearch.js
+++ recipe-app/src/components/RecipeSearch.js
@@ -1,38 +1,31 @@
-import React, { Component } from 'react';
-import RecipeAPI from './RecipeAPI.js'
+import React, { useState } from 'react';
 
-class RecipeSearch extends Component{
-    constructor(props){
-        super(props)
-        this.state = { query: "" }
+function RecipeSearch(props){
+    
+    const [query, setQuery] = useState('');
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    const handleChange = event => {
+        setQuery(event.target.value);
     }
 
-    handleChange(event){
-        this.setState({ query : event.target.value });
-    }
-
-    handleSubmit(event){
+    const handleSubmit = event => {
         event.preventDefault();
-        let path = 'recipes/?name='+this.state.query
-        this.props.searchRecipes(path);
+        let path = 'recipes/?name='+query
+        props.searchRecipes(path);
     }
 
-    render(){
-        return(
-            <form className="form-inline my-2 my-lg-0" onSubmit={this.handleSubmit}>
-            <input className="form-control mr-sm-2" 
-                   type="search" 
-                   placeholder="Search" 
-                   aria-label="Search" 
-                   value={this.state.query} 
-                   onChange={this.handleChange}/>
-            <button className="btn btn-outline-info my-2 my-sm-0" >Search</button>
-            </form>
-        );
-    }
+    return(
+        <form className="form-inline my-2 my-lg-0" onSubmit={handleSubmit}>
+        <input className="form-control mr-sm-2" 
+                type="search" 
+                placeholder="Search" 
+                aria-label="Search" 
+                value={query} 
+                onChange={handleChange}/>
+        <button className="btn btn-outline-info my-2 my-sm-0" >Search</button>
+        </form>
+    );
+
 }
 
 export default RecipeSearch;
