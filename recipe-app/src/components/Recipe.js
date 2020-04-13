a1f5872c255700d27578ce6df2a15fde8fc27c30
eballo <enric.ballo@travelperk.com>
2020-04-13 11:07:41 +0200

    Recipe to functional

--- recipe-app/src/components/Recipe.js
+++ recipe-app/src/components/Recipe.js
@@ -3,27 +3,25 @@ import { Link } from "react-router-dom";
 import './Recipe.css';
 import uuid from 'react-uuid'
 
-class Recipe extends Component{
-    render(){
-        return(
-            <div className="Recipe-container">
-                <div className="Recipe-card u-clearfix">
-                    <div className="Recipe-card-body">
-                        <h2 className="Recipe-card-title">{this.props.name}</h2>
-                        <span className="Recipe-card-delete" onClick={()=>this.props.handleRecipeDelete(this.props.id)}><i className="fa fa-trash" aria-hidden="true"></i></span>
-                        <span className="Recipe-card-description subtle">{this.props.description}</span>
-                        <div className="Recipe-card-read">
-                            <Link to={'/recipes/view/'+this.props.id} >Read</Link>
-                        </div>
-                            {this.props.ingredients.map((p) =>(
-                                <span key={uuid()} className="subtle">{p.name} </span>
-                            ))}
+function Recipe(props){
+    return(
+        <div className="Recipe-container">
+            <div className="Recipe-card u-clearfix">
+                <div className="Recipe-card-body">
+                    <h2 className="Recipe-card-title">{props.name}</h2>
+                    <span className="Recipe-card-delete" onClick={()=>props.handleRecipeDelete(props.id)}><i className="fa fa-trash" aria-hidden="true"></i></span>
+                    <span className="Recipe-card-description subtle">{props.description}</span>
+                    <div className="Recipe-card-read">
+                        <Link to={'/recipes/view/'+props.id} >Read</Link>
                     </div>
+                        {props.ingredients.map((p) =>(
+                            <span key={uuid()} className="subtle">{p.name} </span>
+                        ))}
                 </div>
-                <div className="Recipe-card-shadow"></div>
             </div>
-            );
-    }
+            <div className="Recipe-card-shadow"></div>
+        </div>
+        );
 }
 
 export default Recipe;
