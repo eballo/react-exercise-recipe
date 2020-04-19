e5be9b5e650c1b7f5dcdc711c621cc273b6aa122
eballo <enric.ballo@gmail.com>
2020-04-19 12:34:21 +0200

    remove unused import

--- recipe-app/src/components/Recipe.js
+++ recipe-app/src/components/Recipe.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import '../styles/Recipe.css';
-import uuid from 'react-uuid'
 
 export default function Recipe(props){
     return(
